45d392d77e70db7e2b581c4a9db012743e3c12af
Mats Sørensen Bratvold <mats.bratvold@gmail.com>
2024-03-18 13:21:05 +0100

    Adding proper false positive and false negative generation on both datasets

--- src/result_analysis.py
+++ src/result_analysis.py
@@ -24,7 +24,8 @@ def print_padded_text(text: str, total_length = 50):
     print(f"{'#': <{padding_length_left}}{text}{'#': >{padding_length_right}}")
 
 def generate_error_analysis_report(
-    results: Iterable[pd.DataFrame],
+    data: pd.DataFrame,
+    predictions: Iterable[pd.DataFrame],
     model_names: Iterable[str],
     folder_path: str = None,
     label_column_name = "Verdict",
@@ -37,23 +38,14 @@ def generate_error_analysis_report(
     false_negative_ids = set()
     overlapping_false_positive_ids = None
     overlapping_false_negative_ids = None
-    filenames = ["false_negatives.csv", "false_positives.csv"]
-    for name, result in zip(model_names, results):
+    for name, prediction in zip(model_names, predictions):
         print("#" * 50)
         print_padded_text(name)
-        # Print empty predictions
-        empty_prediction_count = len(result[result["score"].isna()])
-        wrong_format_count = len(result[result["reasoning"].map(lambda x: x is None or "####" in str(x))])
-        false_negatives = result[result.apply(lambda x: x[label_column_name] == 1 and x["score"] < 30, axis=1)]
-        false_positives = result[result.apply(lambda x: x[label_column_name] == 0 and x["score"] > 70, axis=1)]
+        prediction[label_column_name] = data.loc[prediction.index][label_column_name]
+        false_negatives = prediction[prediction.apply(lambda x: x["prediction"]==0 and x[label_column_name]==1, axis=1)]
+        false_positives = prediction[prediction.apply(lambda x: x["prediction"]==1 and x[label_column_name]==0, axis=1)]
         print_padded_text(f"False positives: {len(false_positives)}")
         print_padded_text(f"False negatives: {len(false_negatives)}")
-        print_padded_text(f"Empty predictions: {empty_prediction_count}")
-        print_padded_text(f"Wrong output format: {wrong_format_count}")
-        if folder_path is not None:
-            for filename, data in zip(filenames, [false_negatives, false_positives]):
-                os.makedirs(f"{folder_path}/{name}/zeroshot/", exist_ok=True)
-                data.to_csv(f"{folder_path}/{name}/zeroshot/{filename}")
         false_positive_ids.update(false_positives.index)
         false_negative_ids.update(false_negatives.index)
         if overlapping_false_positive_ids is None:
@@ -71,22 +63,22 @@ def generate_error_analysis_report(
     print_padded_text(f"Overlapping false positives: {len(overlapping_false_positive_ids)}")
     print_padded_text(f"Overlapping false negatives: {len(overlapping_false_negative_ids)}")
     print("#" * 50)
+    filenames = ["false_negatives.csv", "false_positives.csv"]
     for filename, ids in zip(filenames, [overlapping_false_negative_ids, overlapping_false_positive_ids]):
         os.makedirs(f"{folder_path}/errors/", exist_ok=True)
         filtered_data = [
             result[result.index.isin(ids)]
-            for result in results
+            for result in predictions
         ]
         filtered_data = [
-            data.rename(columns={"score": f"{model_name}_score", "reasoning": f"{model_name}_reasoning"})
+            data.rename(columns={"prediction": f"{model_name}_prediction"})
             for data, model_name in zip(filtered_data, model_names)
         ]
-        all_data = pd.concat(filtered_data, axis=1)
+        all_data = pd.concat([*filtered_data, data[text_column_name]], axis=1)
         columns = [
             label_column_name, 
             text_column_name, 
-            *[f"{name}_score" for name in model_names], 
-            *[f"{name}_reasoning" for name in model_names]
+            *[f"{name}_prediction" for name in model_names], 
         ]
         all_data = all_data.loc[:, ~all_data.columns.duplicated()][columns]
         all_data.to_csv(f"../results/{folder_path}/errors/{filename}")
